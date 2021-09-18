c794df1515637af00b349899eb289885a69bcfe8
Sandro Junqui <sandrojunqui@gmail.com>
2021-09-18 15:53:28 -0300

    Tratando erros com exceção personalizada

--- xadrez-console/Tabuleiro/Tabuleiro.cs
+++ xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -17,11 +17,43 @@
         {
             return pecas[linha, coluna];
         }
+
+        public Peca peca(Posicao pos)   // sobrecarga
+        {
+            return pecas[pos.Linha, pos.Coluna];
+        }
         
+        public bool ExistePeca(Posicao pos) // verifica se existe uma peça na posição
+        {
+            ValidarPosicao(pos);
+            return peca(pos) != null;
+        }
+
         public void ColocarPeca(Peca p, Posicao pos)
         {
+            if (ExistePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
             pecas[pos.Linha, pos.Coluna] = p;   // coloca a peça na matriz
             p.posicao = pos;    // agora a peça esta na posição pos
+        }
+
+        public bool PosicaoValida(Posicao pos)  // retorna se a posição é válida ou não
+        {
+            if(pos.Linha < 0 || pos.Linha >= Linhas || pos.Coluna < 0 || pos.Coluna >= Colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidarPosicao(Posicao pos)  // validar uma posição e retorna uma excessão
+        {
+            if (!PosicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
         }
     }
 }
